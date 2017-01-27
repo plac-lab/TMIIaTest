b22fb0f30d30a58e69feb8e9285a49f08364d0c1
pyxiong <pxiong@lbl.gov>
2017-01-26 18:20:13 -0800

    Update SR_Control_tb.v

--- Firmware/test_bench/tm_shiftreg_rw/SR_Control_tb.v
+++ Firmware/test_bench/tm_shiftreg_rw/SR_Control_tb.v
@@ -1,5 +1,4 @@
 `timescale 1ns / 1ps
-`define WIDTH 170
 //////////////////////////////////////////////////////////////////////////////////
 // Company: ccnu
 // Engineer: Poyi Xiong
@@ -23,20 +22,22 @@
 
 module SR_Control_tb();
 
-reg [`WIDTH-1:0] din;
+reg [WIDTH-1:0] din;
 reg clk;
 reg rst;
 reg start;
-wire [7:0] count;
+wire clk_sr;
 wire din_sr;
 wire load_sr;
+
+parameter WIDTH=170;
 
 SR_Control DUT2(
     .din(din),
     .clk(clk),
     .rst(rst),
     .start(start),
-    .count(count),
+    .clk_sr(clk_sr),
     .din_sr(din_sr),
     .load_sr(load_sr)
     );
@@ -57,7 +58,7 @@ rst=1;
 end
 
 initial begin
-din=`WIDTH'b1011;
+din={1'b1,169'b1011};
 start=0;
 #450 start=1;
 #100 start=0;
